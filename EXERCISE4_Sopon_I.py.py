220b9dbbb97218661c572ad16593e9e5731df043
insuwan2410 <52350386+insuwan2410@users.noreply.github.com>
2019-08-04 20:57:11 +0700

    Add files via upload

---
+++ EXERCISE4_Sopon_I.py.py
@@ -0,0 +1,13 @@
+f="Foundation English:"
+FE=88
+g="General Business:"
+GB=63
+i="Introduction to Computer systems:"
+ICS=65
+C="Computer Programming:"
+CR=70
+print("-----your Score-----")
+print(f,FE)
+print(g,GB)
+print(i,ICS)
+print(C,CR)
