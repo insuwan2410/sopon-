c8dae552efd9933209826f837a35bdecac2e11fa
insuwan2410 <52350386+insuwan2410@users.noreply.github.com>
2019-09-14 19:36:06 +0700

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1.
+
+# In[4]:
+
+
+k=hello
+while k=5
+         print(k)
+         k+=5
+        print(k)
+
+
+# In[6]:
+
+
+k=hello
+while k=5
+         print(k)
+         k+=5
+        print(k)
+
+
+# In[3]:
+
+
+i = (hello)
+
+while i = 5:
+    print(i, end = ', ')
+    i=i+1
+    
+
+
+# In[6]:
+
+
+x=0
+while x <5:
+    print('Loop ที่ ', hello)
+    x =5
+print(' Loop hello')
+
+
+# # 3
+
+# In[ ]:
+
+
+
+
